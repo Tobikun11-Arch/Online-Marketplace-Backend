7fb1077acb0a01b0af3c9e503240fa9753d8c1a8
Joenel <joenelsevellejo831@gmail.com>
2024-12-29 20:27:48 +0800

    deleted the search history products

--- user/userDataHandler.ts
+++ user/userDataHandler.ts
@@ -2,30 +2,42 @@ import { Request, Response } from 'express';
 import User from '../models/user';
 
 export const SearchList = async (req: Request, res: Response) => {
-    const { userId, search } = req.body;
-    try {
-        const dispose_exceed = await User('buyer').findById(userId) //Find if user exist on db
+    const { userId, search, deleteItem } = req.body;
 
-        if(dispose_exceed) {
-            const user = await User('buyer').updateOne(
-                {
-                    _id: userId
-                }, {
-                    $push: { 
-                        SearchData: {
-                            $each: [search],
-                            $slice: -20
+    try {
+        const user_exist = await User('buyer').findById(userId)
+        if(user_exist) {
+            if(deleteItem) {
+                    const user = await User('buyer').updateOne(
+                        {
+                            _id: userId
+                        }, {
+                            $pull: {
+                                SearchData: deleteItem
+                            }
                         }
+                    )
+                    if(user.modifiedCount > 0){
+                        return res.status(200).json({ message: "search deleted" })
+                    } else {
+                    const user = await User('buyer').updateOne(
+                        {
+                            _id: userId
+                        }, {
+                            $push: { 
+                                SearchData: {
+                                    $each: [search],
+                                    $slice: -20
+                                }
+                            }
+                        }
+                    )
+                    if(user.modifiedCount > 0){
+                        return res.status(200).json({ message: "new search added" })
+                    } else {
+                        return res.status(200).json({ message: "Guess only" })
                     }
                 }
-            )
-    
-            if(user.modifiedCount > 0){
-                return res.status(200).json({ message: "new search added" })
-            }
-    
-            else {
-                return res.status(200).json({ message: "Guess only" })
             }
         }
     } catch (error) {
