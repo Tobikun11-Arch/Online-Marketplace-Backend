b07e54f4694168b64206d84a9aa502de4878212a
Joenel <joenelsevellejo831@gmail.com>
2025-01-30 01:17:57 +0800

    products order in seller ✅

--- user/userDataHandler.ts
+++ user/userDataHandler.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import User from '../models/user';
 import Product from '../models/product';
-import { Types } from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 import { product_types_stripe } from '../types/stripeProduct';
 const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET);
 
@@ -99,188 +99,207 @@ export const StripePayment = async (req: Request, res: Response) => {
 
 export const Orders = async (req: Request, res: Response) => {
     const { userId, cartProducts } = req.body
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
     try {
         const user_exist = await User('buyer').findById(userId)
         if (!user_exist) {
             return res.status(404).json({ message: "User not found" });
         }
-        if(user_exist) {
-            if(cartProducts) {       
-                const product_Ids = Array.isArray(cartProducts) ? cartProducts : [cartProducts]
 
-                console.log("Product id: ", product_Ids)
-                const productCart = await User('buyer').findOne(
-                    {
-                        _id: userId,
-                        cart: {
-                            $elemMatch: {
-                                productId: { $in: product_Ids }
-                            }
-                        }
-                    }
-                )
-               
+        if(!cartProducts) {  
+            return res.status(400).json({ message: "No products found" })
+        }     
+        const product_Ids = Array.isArray(cartProducts) ? cartProducts : [cartProducts] //this one that will need to put in id instead of test id
 
-                const products = await Product.findOne(
-                    {
-                        _id: cartProducts
-                    }
-                )
-
-                if (productCart && productCart.cart.length > 0 ) { //for buying from cart
-                    const newOrder = productCart?.cart.map(cart => ({
-                        productId: cart.productId,
-                        productName: cart.productName,
-                        images: cart.images,
-                        quantity: cart.quantity,
-                        price: cart.price
-                    }))
-
-
-                     //Start test           
-                    const test_id =  [ '678f72497e911ffa2d947240' ]  
-
-                    const buyer_information = await User('buyer').findOne({_id: userId}) 
-                    console.log("buyer info: ", buyer_information) //buyer info
-
-                    if(!buyer_information) {
-                        return res.status(404).json({ message: "Buyer not found" })
-                    }
-
-                    console.log("Order 1: ", newOrder) //order details
-
-
-                    const search_id_seller = await User('seller').findOne({ //change the test id later for actual id, if i already refactor the fetch in frontend
-                        sellerProducts: {
-                            $elemMatch: {
-                                _id: { $in: test_id }
-                            }
-                        }
-                    }) 
-
-                    if(!search_id_seller) {
-                        return res.status(404).json({ message: "Product not found" })
-                    }
-
-                    const add_on_orderlist = await User('seller').updateOne({  _id: search_id_seller._id }, {
-                        $push: {
-                            product_orders: {
-                                buyer_firstName: buyer_information.FirstName,
-                                buyer_lastName: buyer_information.LastName,
-                                buyer_email: buyer_information.Email,
-                                buyer_username: buyer_information.Username,
-                                product: newOrder
-                            }
-                        }
-                    })
-
-                    if(add_on_orderlist.modifiedCount > 0) {
-                        console.log("Order list updated")
-                    } else {
-                        console.log("Order list not updated")
-                    }
-                    //End test
-
-                    const totalAmount = newOrder.reduce((sum, item) => sum + Number(item.price) * Number(item.quantity), 0);
-                    const order = {
-                        orderId: new Types.ObjectId(),
-                        items: newOrder,
-                        totalAmount: totalAmount,
-                        status: 'purchased', 
-                        createdAt: new Date(),
-                    }
-
-                    const add_order = await User('buyer').updateOne(
-                        {
-                            _id: userId
-                        }, {
-                            $push: {
-                                orders: order
-                            }
-                        }
-                    )
-                    const remove_cart = await User('buyer').updateOne(
-                    {
-                        _id: userId
-                    }, {
-                        $pull: {
-                            cart: {
-                                productId: { $in: cartProducts }
-                            }
-                        }
-                    })
-                    if(remove_cart.modifiedCount > 0 && add_order.modifiedCount > 0) {
-                        return res.status(200).json({ message: "Update success" })
-                    }
-                } else { //direct buy
-                    const Items = {
-                        productId: products?._id,
-                        productName: products?.productName,
-                        images: products?.images,
-                        quantity: 1,
-                        price: products?.productPrice
-                    }
-
-                       //Start test           
-                       const test_id =  [ '678f72497e911ffa2d947240' ] //product_id i want to find in sellerproducts in seller
-                       //sending product details to seller sellerproducts[]
-                       const buyer_information = await User('buyer').findOne({_id: userId})
-                       console.log("buyer info: ", buyer_information)
-                       
-   
-                       const search_id_seller = await User('seller').findOne({ //change the test id later for actual id, if i already refactor the fetch in frontend
-                           sellerProducts: {
-                               $elemMatch: {
-                                   _id: { $in: test_id }
-                               }
-                           }
-                       }) 
-   
-                       if(!search_id_seller) {
-                           return res.status(404).json({ message: "Product not found" })
-                       }
-   
-                       const matchingProducts = search_id_seller.sellerProducts.filter(product => 
-                           test_id.includes(product._id.toString())
-                       );
-   
-                       console.log("quantity: 1")
-                       
-                       console.log("Found: ", matchingProducts);
-                       //End test
-   
-
-                    const order = {
-                        orderId: new Types.ObjectId(),
-                        items: Items,
-                        totalAmount: 0,
-                        status: 'purchased', 
-                        createdAt: new Date(),
-                    }
-
-                    console.log("Order 2: ", order)
-
-                    const add_order = await User('buyer').updateOne(
-                        {
-                            _id: userId
-                        }, {
-                            $push: {
-                                orders: order
-                            }
-                        }
-                    )
-
-                    if(add_order.modifiedCount > 0) {
-                        return res.status(200).json({ message: "Update success" })
+        const productCart = await User('buyer').findOne(
+            {
+                _id: userId,
+                cart: {
+                    $elemMatch: {
+                        productId: { $in: product_Ids }
                     }
                 }
             }
-        }
-        else {
-            return res.status(401).json({ message: "Unathorized user" })
+        ).session(session)
+
+        const products = await Product.findOne(
+            {
+                _id: cartProducts
+            }
+        ).session(session)
+
+        if (productCart && productCart.cart.length > 0 ) { //for buying from cart
+            const newOrder = productCart?.cart.map(cart => ({
+                productId: cart.productId,
+                productName: cart.productName,
+                images: cart.images,
+                quantity: cart.quantity,
+                price: cart.price
+            }))
+
+
+            //Start test... Refactor this later           
+            const test_id =  [ '678f72497e911ffa2d947240' ]  //change the test id later for actual id, if i already refactor the fetch in frontend
+
+            const buyer_information = await User('buyer').findOne({_id: userId}) //buyer info
+            if(!buyer_information) {
+                await session.abortTransaction()
+                session.endSession()
+                return res.status(404).json({ message: "Buyer not found" })
+            }
+
+            //searching whos seller for that product, i use product Id to find the seller, it uses test id because i dont have the actual id yet coming from frontend
+            const search_id_seller = await User('seller').findOne({ 
+                sellerProducts: {
+                    $elemMatch: {
+                        _id: { $in: test_id }
+                    }
+                }
+            }).session(session)
+
+            if(!search_id_seller) {
+                await session.abortTransaction();
+                session.endSession();
+                return res.status(404).json({ message: "Product not found" })
+            }
+
+            //adding buyer and order info on product_orders in seller
+            const add_on_orderlist = await User('seller').updateOne({  _id: search_id_seller._id }, 
+                {
+                    $push: {
+                        product_orders: {
+                            buyer_firstName: buyer_information.FirstName,
+                            buyer_lastName: buyer_information.LastName,
+                            buyer_email: buyer_information.Email,
+                            buyer_username: buyer_information.Username,
+                            product: newOrder
+                        }
+                    }
+                }, { session }
+            )
+            //End test
+
+            const totalAmount = newOrder.reduce((sum, item) => sum + Number(item.price) * Number(item.quantity), 0);
+            const order = {
+                orderId: new Types.ObjectId(),
+                items: newOrder,
+                totalAmount: totalAmount,
+                status: 'purchased', 
+                createdAt: new Date(),
+            }
+
+            const add_order = await User('buyer').updateOne(
+                {
+                    _id: userId
+                }, {
+                    $push: {
+                        orders: order
+                    }
+                },
+                { session }
+            )
+            const remove_cart = await User('buyer').updateOne(
+            {
+                _id: userId
+            }, {
+                $pull: {
+                    cart: {
+                        productId: { $in: cartProducts }
+                    }
+                }
+            }, { session })
+            if(remove_cart.modifiedCount > 0 && add_order.modifiedCount > 0 && add_on_orderlist.modifiedCount > 0) {
+                await session.commitTransaction()
+                session.endSession()
+                return res.status(200).json({ message: "Update success" })
+            } else {
+                await session.abortTransaction()
+                session.endSession()
+                return res.status(400).json({ message: "Failed to update" })
+            }
+        } else { //direct buy
+            const Items = {
+                productId: products?._id,
+                productName: products?.productName,
+                images: products?.images,
+                quantity: 1,
+                price: products?.productPrice
+            }
+
+                //Start test... Refactor this later           
+            const test_id =  [ '678f72497e911ffa2d947240' ]  //change the test id later for actual id, if i already refactor the fetch in frontend
+
+            const buyer_information = await User('buyer').findOne({_id: userId}).session(session) //buyer info
+            if(!buyer_information) {
+                await session.abortTransaction()
+                session.endSession()
+                return res.status(404).json({ message: "Buyer not found" })
+            }
+
+            //searching whos seller for that product, i use product Id to find the seller, it uses test id because i dont have the actual id yet coming from frontend
+            const search_id_seller = await User('seller').findOne({ 
+                sellerProducts: {
+                    $elemMatch: {
+                        _id: { $in: test_id }
+                    }
+                }
+            }).session(session)
+
+            if(!search_id_seller) {
+                await session.abortTransaction()
+                session.endSession()
+                return res.status(404).json({ message: "Product not found" })
+            }
+
+            //adding buyer and order info on product_orders in seller
+            const add_on_orderlist = await User('seller').updateOne({  _id: search_id_seller._id }, {
+                $push: {
+                    product_orders: {
+                        buyer_firstName: buyer_information.FirstName,
+                        buyer_lastName: buyer_information.LastName,
+                        buyer_email: buyer_information.Email,
+                        buyer_username: buyer_information.Username,
+                        product: Items
+                    }
+                }
+            }, { session })
+            //End test
+
+            const order = {
+                orderId: new Types.ObjectId(),
+                items: Items,
+                totalAmount: 0,
+                status: 'purchased', 
+                createdAt: new Date(),
+            }
+
+            const add_order = await User('buyer').updateOne(
+                {
+                    _id: userId
+                }, {
+                    $push: {
+                        orders: order
+                    }
+                }, { session }
+            )
+
+            if(add_order.modifiedCount > 0 && add_on_orderlist.modifiedCount > 0) {
+                await session.commitTransaction()
+                session.endSession()
+                return res.status(200).json({ message: "Update success" })
+            } else {
+                await session.abortTransaction()
+                session.endSession()
+                return res.status(400).json({ message: "Failed to update" })
+            }
         }
     } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
         console.error(error)
+        return res.status(500).json({ message: "Internal server error" })
     }
     
 }
